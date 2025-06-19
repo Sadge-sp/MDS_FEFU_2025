0e292750e11be8d242d15c9321100838c141d19a
samyon <samsuperman.gtarp@gmail.com>
2025-06-19 21:45:13 +1000

    псевдо рабочий

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import boto3
@@ -40,6 +40,59 @@ def test_redis():
     redis_client.set('test_key', 'test_value')
     value = redis_client.get('test_key').decode('utf-8')
     return jsonify({'message': f'Redis value: {value}'}), 200
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+# Страница входа
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Страница создания аккаунта
+@app.route('/register')
+def register():
+    return render_template('creatacc.html')
+
+# Страница конструктора
+@app.route('/configurator')
+def configurator():
+    return render_template('buildpage.html')
+
+# Страница корзины
+@app.route('/cart')
+def cart():
+    return render_template('card.html')
+
+# Страница доставки
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+# Страница отслеживания заказа
+@app.route('/track')
+def track():
+    return render_template('ordertsking.html')
+
+# Страница поддержки
+@app.route('/support')
+def support():
+    return render_template('suport.html')
+
+# Страница профиля мыши
+@app.route('/mouse_profile')
+def mouse_profile():
+    return render_template('mouse_profile.html')
+
+# Страница фильтра мышей
+@app.route('/catalog')
+def catalog():
+    return render_template('filter_mouse.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5001, debug=True)
